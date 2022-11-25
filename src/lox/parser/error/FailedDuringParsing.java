732840986e04f4f4f92ecc5bdc0b9583411a9b56
Octave Larose <octave.larose@hotmail.fr>
2022-11-25 13:46:10 +0000

    Parsing failed instead of a dubious error message

--- src/lox/parser/error/FailedDuringParsing.java
+++ src/lox/parser/error/FailedDuringParsing.java
@@ -1,10 +1,8 @@
 package lox.parser.error;
 
-import lox.parser.Token;
-
 public class FailedDuringParsing extends Exception {
     @Override
     public String getMessage() {
-        return "super mario 64";
+        return "Parsing failed";
     }
 }
